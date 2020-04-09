05c426fe8d94cad324816974b4d91ea9c4ad0de9
Χρήστος Κοτρώτσιος <43947668+chriskotrotsios@users.noreply.github.com>
2020-04-09 19:46:27 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import csv
+import MySQLdb
+
+covid19 = MySQLdb.connect(host='localhost',
+    user='root',
+    passwd='',
+    db='covid19')
+cursor = covid19.cursor()
+csv_data = csv.reader(open('covid_19_data.csv'))
+for row in csv_data:
+
+    cursor.execute('INSERT INTO countries(SNo, ObservationDate, Province_State, Country_Region, Last_Update, Confirmed, Deaths, Recovered )' \
+          'VALUES("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', 
+          row)
+#close the connection to the database.
+covid19.commit()
+cursor.close()
+print ("Done")
