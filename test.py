dc822edad97d109c24f955c75930d722cef09f2e
Dimitris Misailidis <43905747+dmisa@users.noreply.github.com>
2020-04-09 20:11:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ csv_data = csv.reader(open('covid_19_data.csv'))
 for row in csv_data:
 
     cursor.execute('INSERT INTO countries(SNo, ObservationDate, Province_State, Country_Region, Last_Update, Confirmed, Deaths, Recovered )' \
-          'VALUES("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', 
+          'VALUES("%s", STR_TO_DATE("%s","%d/%m/%Y"), "%s", "%s", "%s", "%s", "%s", "%s")', 
           row)
 #close the connection to the database.
 covid19.commit()
